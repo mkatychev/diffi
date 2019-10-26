67b1b403a4148c060e98710b5f531bb6cc1f7ba5
Mikhail Katychev <mkatych@gmail.com>
2019-10-26 15:24:33 -0500

    more diffi path corner cases

--- git-diffi.sh
+++ git-diffi.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
-
-# set -x
+git config --global alias.diffi '!git diff'
 _git_root=$(git rev-parse --show-toplevel)
 
 test -f "$_git_root/.diffignore" || \
     cp "./$_git_root/.diffignore.base" "./$_git_root/.diffignore" >& /dev/null || \
     >&2 echo "diffignore: no .diffignore or .diffignore.base found in root git directory!"|| exit 1
 
-PATHS=""
-VALID_ARGS=""
+PATHS=()
+VALID_ARGS=()
 
 while (( "$#" )); do
     # swallow double dash since we will append it anyways
@@ -16,9 +15,9 @@ while (( "$#" )); do
         continue
     # determine valid glob patterns also checks existence of strict path
     elif [[ $(compgen -G "$1") ]]; then
-        PATHS+=" $1"
+        PATHS+=("$1")
     else
-        VALID_ARGS+=" $1"
+        VALID_ARGS+=("$1")
     fi
 shift
 done
@@ -27,18 +26,23 @@ done
 
 
 # https://git-scm.com/docs/gitignore#_pattern_format
-DIFFI=""
+DIFFI=()
 
 
 
 while read -r ignore;do
     if [ -n "$ignore" ] && [[ "${ignore}" != \!* ]] && [[ "$ignore" != \#* ]] ;
     then
-        DIFFI+=" :!${ignore}"
+        DIFFI+=(":!${ignore}")
     fi
 done < "$_git_root/.diffignore"
 
 
 
+set -x 
 # git diff "$@" -- $(scripts/diffignore.sh)
-echo "$VALID_ARGS -- $(realpath -z $PATHS)$DIFFI"
+[[ -n "$PATHS" ]] && REAL_PATHS=$(realpath --relative-to=$_git_root ${PATHS[@]})
+# execute git diff from repo root so that diffignore is applied properly
+# keeping provided direct paths
+(cd $_git_root; \
+    git diff $(echo "${VALID_ARGS[@]} --  ${REAL_PATHS:-} ${DIFFI[@]}"))
